98e50a624da281756aa5e0772bd785c226827575
Chris Streeter <chris@chrisstreeter.com>
2011-05-28 12:59:14 -0700

    Login and logout.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('app.views',
+    url(r'^login/$', 'login', name='login'),
+    url(r'^logout/$', 'logout', name='logout'),
+    url(r'^setup/$', 'setup', name='setup'),
     url(r'^$', 'landing', name='landing'),
 )
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from annoying.decorators import render_to
+from google.appengine.api import users
+from account.decorators import login_required
 
 import logging
 log = logging.getLogger('gallery.' + __name__)
@@ -8,10 +10,29 @@ log = logging.getLogger('gallery.' + __name__)
 
 @render_to('landing.html')
 def landing(request):
-    return {}
+    return {'title': 'Your Gallery'}
 
 
 @login_required
-@render_to('about/privacy.html')
-def privacy(request):
-    return {}
+@render_to('setup.html')
+def setup(request):
+    return {'title': 'Setup Your Gallery'}
+
+
+def login(request):
+    user = users.get_current_user()
+    
+    if not user:
+        next = request.GET.get('next', '/')
+        return HttpResponseRedirect(users.create_login_url(next))
+    else:
+        return HttpResponseRedirect('landing')
+
+
+def logout(request):
+    user = users.get_current_user()
+    if user:
+        next = request.GET.get('next', '/')
+        return HttpResponseRedirect(users.create_logout_url(next))
+    else:
+        return HttpResponseRedirect('landing')
