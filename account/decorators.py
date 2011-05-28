7a213e42d69e20acdb1131e75b473e6136be60f0
Chris Streeter <chris@chrisstreeter.com>
2011-05-28 12:58:40 -0700

    Add login_required decorator.

---
+++ account/decorators.py
@@ -0,0 +1,25 @@
+try:
+    from functools import wraps
+except ImportError:
+    from django.utils.functional import wraps  # Python 2.4 fallback.
+
+from django.core.urlresolvers import reverse
+from django.utils.decorators import available_attrs
+from django.http import HttpResponseRedirect
+from google.appengine.api import users
+
+
+def login_required(function=None):
+    
+    def decorator(view_func):
+        @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
+            if users.get_current_user():
+                return view_func(request, *args, **kwargs)
+            next = request.GET.get('next', '/')
+            return HttpResponseRedirect(users.create_logout_url(next))
+        return _wrapped_view
+    
+    if function:
+        return decorator(function)
+    return decorator
