af298c92c4f92511f0d3257ff965166d894f4251
Chris Streeter <chris@chrisstreeter.com>
2011-05-28 09:13:03 -0700

    Add about pages.

--- about/views.py
+++ about/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.conf import settings
+from annoying.decorators import render_to
+
+import logging
+log = logging.getLogger('gallery.' + __name__)
+
+
+@render_to('about/terms.html')
+def terms(request):
+    return {}
+
+
+@render_to('about/privacy.html')
+def privacy(request):
+    return {}
+
+
+@render_to('about/about.html')
+def about(request):
+    return {}
