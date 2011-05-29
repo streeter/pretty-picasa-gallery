edf4ead2eaac8d9462c58dabc08c54cfab39ccea
Chris Streeter <chris@chrisstreeter.com>
2011-05-29 12:08:17 -0700

    Initial account model.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+from google.appengine.ext import db
+from app import (BACKENDS, PICASA_BACKEND, FLICKR_BACKEND,
+    THUMB_SIZES, THUMB_SIZE_DEFAULT, THUMB_CROPPED_DEFAULT,
+    FULL_SIZES, FULL_SIZE_DEFAULT, HOMEPAGE_SIZE_DEFAULT, )
 
-# Create your models here.
+
+class UserPrefs(db.Model):
+    user = db.UserProperty(required=True)
+    
+    photo_backend = db.StringProperty(choices=BACKENDS, default=PICASA_BACKEND,
+        required=True)
+    
+    site_title = db.StringProperty(default='photo')
+    site_header = db.StringProperty(default='photo')
+    
+    thumb_size = db.IntegerProperty(choices=THUMB_SIZES, default=THUMB_SIZE_DEFAULT, required=True)
+    thumb_cropped = db.BooleanProperty(default=THUMB_CROPPED_DEFAULT, required=True)
+    
+    full_size = db.IntegerProperty(choices=FULL_SIZES, default=FULL_SIZE_DEFAULT, required=True)
+    
+    homepage_size = db.IntegerProperty(choices=FULL_SIZES, default=HOMEPAGE_SIZE_DEFAULT, required=True)
+    homepage_album = db.StringProperty()
+    
+    featured_albums = db.StringListProperty()
+    
+    picasa_id = db.StringProperty(default='default')
+    flickr_id = db.StringProperty()
+    
+    merchant_id = db.StringProperty()
+    analytics_id = db.StringProperty()
+    
+    def _get_service_username(self):
+        if self.photo_backend == PICASA_BACKEND:
+            return self.picasa_id
+        elif self.photo_backend == FLICKR_BACKEND:
+            return self.flickr_id
+        else:
+            return None
+    def _set_service_username(self, id):
+        if self.photo_backend == PICASA_BACKEND:
+            self.picasa_id = id
+        elif self.photo_backend == FLICKR_BACKEND:
+            self.flickr_id = id
+        else:
+            return None
+    service_username = property(_get_service_username, _set_service_username)
+    
+    def _backend(self):
+        if self.photo_backend == PICASA_BACKEND:
+            return PicasaBackend(self)
+        elif self.photo_backend == FLICKR_BACKEND:
+            return FlickrBackend(self)
+        else:
+            return None
+    backend = property(_backend)
